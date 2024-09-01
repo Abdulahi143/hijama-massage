e29775aa6555e9f91d02528d654b482b5f36b8f6
Abdulahi Shakur <shakurjr@Abdulahis-MacBook-Pro.local>
2024-09-01 02:24:47 +0200

    Done the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+"use client"
+
+import "aos/dist/aos.css";
+
+import AOS from "aos";
+import Hero from "./_components/Hero";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <div>Hijama Massage</div>;
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      mirror: false
+    });
+  }, []);
+  return (
+    <>
+    <Hero />
+    </>
+  );
 }
